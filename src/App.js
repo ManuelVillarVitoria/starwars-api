209468b9852ceb73b30ebb3d9b418136f04aa132
Manu <manu.villar.vitoria@gmail.com>
2022-03-20 23:33:15 +0100

    displaying list of more characters clicking on the name of main list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { getPeople } from './api/people';
+import { getCharacter, getPeople } from './api/people';
 
 
 
-function App() {
+const  App = () => {
 
   const [people, setPeople] = useState([]);
   const [errorState, setErrorState] = useState({ hasError: false});
+  const [currentCharacter, setCurrentCharacter] = useState(1);
+  const [details, setDetails] = useState({});
 
   useEffect(() => {
     getPeople()
@@ -15,19 +17,45 @@ function App() {
     .catch(handleError);
   }, []);
 
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails).catch(handleError);
+  }, [currentCharacter]);
+
+
   const handleError = error => {
     setErrorState({ hasError: true, message: error.message });
-  }
+  };
+
+
+  const showDetails = character => {
+    const id = Number(character.url.split("/").slice(-2)[0]);
+    setCurrentCharacter(id);
+  };
 
 
   return (
-    <ul>
-      {errorState.hasError &&  <div>{errorState.message}</div>}
+    <>
+      <ul>
+        {errorState.hasError &&  <div>{errorState.message}</div>}
 
-      {people.map(character => (
-        <li key={character.name}>{character.name}</li>
-      ))}
-    </ul>
+        {people.map(character => (
+          <li key={character.name} onClick = {() => showDetails(character)}>
+            {character.name}
+          </li>
+        ))}
+      </ul>
+
+      {details && (
+        <aside>
+        <h1>{details.name}</h1>
+        <ul>
+          <li>height: {details.height}</li>
+          <li>mass: {details.mass}</li>
+          <li>Year of Birth: {details.birth_year}</li>
+        </ul>
+       </aside>
+      )}
+    </>
   );
 }
 
--- src/api/people.js
+++ src/api/people.js
@@ -10,9 +10,13 @@ export async function getPeople() {
 
  } catch (error) {
      throw error;
-     
  }
-  
+}
+
+export async function getCharacter(id = 1) {
+    const response = await fetch (`https://swapi.dev/api/people/${id}`);
+    const data =  await response.json();
+    return data;
 }
 
 class NetworkError extends Error {
